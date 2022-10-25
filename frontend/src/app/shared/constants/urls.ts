406655f912b1405c9d0459a7df532910bfb22038
Amol Kajgikar <amolkajgikar@gmail.com>
2022-10-25 12:42:18 +0400

    master

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,4 +1,7 @@
-const BASE_URL='http://localhost:5000';
+import { environment } from './../../../environments/environment';
+// const BASE_URL='http://localhost:5000';
+
+const BASE_URL= environment.production? '' : 'http://localhost:5000';
 
 export const FOODS_URL = BASE_URL + '/api/foods';
 
